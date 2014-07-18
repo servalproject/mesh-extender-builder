ce2c3c35601921694f87000347def5b4dbe6c969
gardners <paul@servalproject.org>
2014-07-18 10:05:08 +0930

    make default IP address 192.168.2.1

--- image-files/lib/functions/uci-defaults.sh
+++ image-files/lib/functions/uci-defaults.sh
@@ -172,7 +172,7 @@ set network.lan='interface'
 set network.lan.ifname='$ifname'
 set network.lan.type='bridge'
 set network.lan.proto='static'
-set network.lan.ipaddr='192.168.1.1'
+set network.lan.ipaddr='192.168.2.1'
 set network.lan.netmask='255.255.255.0'
 EOF
 }
